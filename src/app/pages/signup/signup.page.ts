e447546d1200402d3f40be7c951ece80fecb9310
dn1023 <brunoprotz81@gmail.com>
2025-02-05 13:27:51 +0100

    update: some content updated.

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  alertButtons = ['Action'];
+  users: any;
+  newUser = { name: '', email: '' }; // Model for new user input
 
-  ngOnInit() {
+  constructor(private authService: AuthService) {}
+
+  async ngOnInit() {
+    await this.authService.openDatabase();
+    await this.loadUsers();
+  }
+
+  // Add a new user
+  async addUser() {
+    if (this.newUser.name.trim() && this.newUser.email.trim()) {
+      await this.authService.createUser(
+        {
+          id: '',
+          firstname: this.newUser.name, 
+          lastname: this.newUser.name,
+          birthday: '',
+          pwa: '',
+          email: this.newUser.email,
+          company: '',
+        });
+      this.newUser = { name: '', email: '' }; // Reset input fields
+      await this.loadUsers(); // Refresh user list
+    } else {
+      alert('Please enter both Name and Email.');
+    }
+  }
+
+  // Load all users
+  async loadUsers() {
+    this.users = await this.authService.getAllUsers();
+  }
+
+  // Delete a user
+  async deleteUser(id: string) {
+    await this.authService.deleteUser(id);
+    await this.loadUsers();
   }
 
 }
